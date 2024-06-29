7009820ca996b92925100725efde186c5e8003e5
ritallopes <ritacassia.lino@hotmail.com>
2024-06-29 19:57:30 -0300

    feat-3: create spike with database models and migrations

---
+++ backend/alembic/versions/facc6705dd50_init_setup.py
@@ -0,0 +1,22 @@
+"""init-setup
+
+Revision ID: facc6705dd50
+Revises: 
+Create Date: 2024-06-29 15:39:14.883071
+
+"""
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = 'facc6705dd50'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
