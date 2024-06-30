5c1925cbbd39a7e6ef03b896c5e5f75add755f59
ritallopes <ritacassia.lino@hotmail.com>
2024-06-30 10:31:54 -0300

    feat-9: change var env

--- backend/app/api/db/database.py
+++ backend/app/api/db/database.py
@@ -9,6 +9,8 @@ dotenv.load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv('PGDATABASEURL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+print(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
